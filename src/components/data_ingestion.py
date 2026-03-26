b95d84b186c13fdec3c1af2ddfa22c7f92b793e5
Ronak-Sah <ronaksah75@gmail.com>
2026-03-26 20:54:14 +0530

    experiment tracking code in data ingestion done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,7 @@ import pandas as  pd
 import numpy as np
 import os
 import sys
+import mlflow
 
 class DataIngestion():
     def __init__(self,config:DataIngestionConfig):
@@ -20,33 +21,50 @@ class DataIngestion():
 
     def load_data(self):
         try:
-            logging.info("Exporting data from MongoDB to Dataframe")
-            df=self.mongo_db.fetch_data()
+            mlflow.set_tracking_uri("file:///D:/Ml Dl/Project/mlops-project/mlruns")
+            mlflow.set_experiment("Data Ingestion")
+            with mlflow.start_run():
 
-            if df.empty:
-                logging.warning("Fetched DataFrame is empty. Check MongoDB collection.")
-                return
-            
-            shuffled_indices=np.random.permutation(len(df))
+                logging.info("Exporting data from MongoDB to Dataframe")
+                df=self.mongo_db.fetch_data()
 
-            create_directories([self.config.train_data_path,self.config.test_data_path,self.config.val_data_path])
+                if df.empty:
+                    logging.warning("Fetched DataFrame is empty. Check MongoDB collection.")
+                    return
+                
+                shuffled_indices=np.random.permutation(len(df))
 
-            test_size=int(len(df)*self.config.test_set_size)
-            val_size=int(len(df)*self.config.val_set_size)
-            test_indices=shuffled_indices[:test_size]
-            val_indices=shuffled_indices[test_size:val_size+test_size]
-            train_indices=shuffled_indices[val_size+test_size:]
+                create_directories([self.config.train_data_path,self.config.test_data_path,self.config.val_data_path])
 
-            train_data=df.iloc[train_indices]
-            test_data=df.iloc[test_indices]
-            val_data=df.iloc[val_indices]
+                test_size=int(len(df)*self.config.test_set_size)
+                val_size=int(len(df)*self.config.val_set_size)
 
-            train_data.to_csv(os.path.join(self.config.train_data_path,"train.csv"),index=False)
-            test_data.to_csv(os.path.join(self.config.test_data_path,"test.csv"),index=False)
-            val_data.to_csv(os.path.join(self.config.val_data_path,"val.csv"),index=False)
-            logging.info(f"Training data saved at folder - {self.config.train_data_path}")
-            logging.info(f"Testing data saved at folder - {self.config.test_data_path}")
-            logging.info(f"Validation data saved at folder - {self.config.val_data_path}")
+                mlflow.log_param("train_split_ratio",self.config.train_set_size)
+                mlflow.log_param("test_split_ratio",self.config.test_set_size)
+                mlflow.log_param("val_split_ratio",self.config.val_set_size)
+
+                test_indices=shuffled_indices[:test_size]
+                val_indices=shuffled_indices[test_size:val_size+test_size]
+                train_indices=shuffled_indices[val_size+test_size:]
+
+                train_data=df.iloc[train_indices]
+                test_data=df.iloc[test_indices]
+                val_data=df.iloc[val_indices]
+
+                train_file = os.path.join(self.config.train_data_path, "train.csv")
+                test_file = os.path.join(self.config.test_data_path, "test.csv")
+                val_file = os.path.join(self.config.val_data_path, "val.csv")
+
+                train_data.to_csv(train_file,index=False)
+                test_data.to_csv(test_file,index=False)
+                val_data.to_csv(val_file,index=False)
+
+                mlflow.log_artifact(train_file, artifact_path="ingested_train_data")
+                mlflow.log_artifact(test_file,artifact_path="ingested_test_path")
+
+                logging.info(f"Training data saved at folder - {self.config.train_data_path}")
+                logging.info(f"Testing data saved at folder - {self.config.test_data_path}")
+                logging.info(f"Validation data saved at folder - {self.config.val_data_path}")
         except Exception as e:
             logging.error("Failed to execute load_data")
             raise CustomException(e,sys)
